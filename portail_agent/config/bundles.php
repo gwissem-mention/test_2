08bc662fefe0fc50b52af194e642edc16e76a8cb
Jérémy JARRIE <jeremy.jarrie@sensiolabs.com>
2022-08-11 10:22:01 +0200

    Add flysystem

--- portail_agent/config/bundles.php
+++ portail_agent/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
 ];
