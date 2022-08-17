c2c8d011706274117fbd74e85c9834751eb8f49b
Thomas CAUTRES <thomas.cautres@sensiolabs.com>
2022-08-17 13:24:11 +0200

    [PEL-33] Add FSI radio buttons on offense nature step

--- portail_citoyen/assets/controllers/complaint/offense_nature_controller.js
+++ portail_citoyen/assets/controllers/complaint/offense_nature_controller.js
@@ -3,7 +3,7 @@ import {Controller} from '@hotwired/stimulus';
 export default class extends Controller {
     displayWarningText(event) {
         const element = event.currentTarget;
-        const isActive = element.selectedOptions[0].getAttribute('data-complaint-offense-nature-active');
+        const isActive = element.selectedOptions[0].getAttribute('data-complaint-offense-nature-warning-text');
         let dataElement = document.getElementById("warning-text");
         if (dataElement) {
             dataElement.remove();
@@ -27,12 +27,12 @@ export default class extends Controller {
         }
     }
 
-    displayOtherAABText(event) {
+    displayOtherAABField(event) {
         const element = event.currentTarget;
         const selectedOption = element.selectedOptions[0];
         const isActive = selectedOption.getAttribute('data-complaint-offense-nature-other-aab');
-        const offenseNatureElem = document.getElementById("aab-text");
-        offenseNatureElem.innerHTML = '';
+        const otherAABFieldElem = document.getElementById("other-aab-field");
+        otherAABFieldElem.innerHTML = '';
 
         if (isActive) {
             const form = document.querySelector('form');
@@ -51,7 +51,36 @@ export default class extends Controller {
                     dataElement.innerHTML = data;
                     const domFragment = new DocumentFragment();
                     domFragment.appendChild(dataElement);
-                    offenseNatureElem.replaceWith(domFragment.getElementById("aab-text"));
+                    otherAABFieldElem.replaceWith(domFragment.getElementById("other-aab-field"));
+                });
+        }
+    }
+
+    displayHouseRobberyFields(event) {
+        const element = event.currentTarget;
+        const selectedOption = element.selectedOptions[0];
+        const isActive = selectedOption.getAttribute('data-complaint-offense-nature-house-robbery');
+        const houseRobberyFields = document.getElementById("house-robbery-fields");
+        houseRobberyFields.innerHTML = '';
+
+        if (isActive) {
+            const form = document.querySelector('form');
+            const offenseNatureFieldName = element.getAttribute('name');
+
+            fetch(form.action, {
+                method: form.method,
+                body: new URLSearchParams([[offenseNatureFieldName, selectedOption.value]]),
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            })
+                .then((response) => response.text())
+                .then((data) => {
+                    const dataElement = document.createElement('div');
+                    dataElement.innerHTML = data;
+                    const domFragment = new DocumentFragment();
+                    domFragment.appendChild(dataElement);
+                    houseRobberyFields.replaceWith(domFragment.getElementById("house-robbery-fields"));
                 });
         }
     }
