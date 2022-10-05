d0a7f4a4c4b67529f29d3779086b4495096f4421
Abdelkarim ABDOUNI <abdelkarim.abdouni@sensiolabs.com>
2022-10-05 12:17:41 +0200

    [PEL-207] - add another offense nature type

--- portail_citoyen/src/Enum/OffenseNature.php
+++ portail_citoyen/src/Enum/OffenseNature.php
@@ -6,7 +6,8 @@ enum OffenseNature: int
 {
     case Robbery = 1;
     case Degradation = 2;
-    case Other = 3;
+    case RobberyAndDegradation = 3;
+    case Other = 4;
 
     /**
      * @return array<string, int>
@@ -16,6 +17,7 @@ enum OffenseNature: int
         return [
             'pel.complaint.offense.nature.robbery' => self::Robbery->value,
             'pel.complaint.offense.nature.degradation' => self::Degradation->value,
+            'pel.complaint.offense.nature.robbery.and.degradation' => self::RobberyAndDegradation->value,
             'pel.complaint.offense.nature.other' => self::Other->value,
         ];
     }
