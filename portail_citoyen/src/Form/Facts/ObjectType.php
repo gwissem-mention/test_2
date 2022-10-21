977287bc84f406df250bc3e4e0f1a6b5ebe15ccc
Abdelkarim ABDOUNI <abdelkarim.abdouni@sensiolabs.com>
2022-10-21 10:39:04 +0200

    [PEL-233] - object multimedia description

--- portail_citoyen/src/Form/Facts/ObjectType.php
+++ portail_citoyen/src/Form/Facts/ObjectType.php
@@ -9,7 +9,11 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ObjectType extends AbstractType
 {
@@ -35,6 +39,77 @@ class ObjectType extends AbstractType
                         'max' => 30,
                     ]),
                 ],
-            ]);
+            ])
+        ;
+
+        $builder->get('category')->addEventListener(
+            FormEvents::POST_SUBMIT,
+            function (FormEvent $event) {
+                /** @var ?int $choice */
+                $choice = $event->getForm()->getData();
+                if (is_null($choice)) {
+                    return;
+                }
+                /** @var FormInterface $parent */
+                $parent = $event->getForm()->getParent();
+                $this->addMultimediaCategoryObjectForm($parent, $choice);
+            }
+        );
+    }
+
+    private function addMultimediaCategoryObjectForm(FormInterface $form, int $choice): void
+    {
+        $choices = $this->objectCategoryThesaurusProvider->getChoices();
+
+        if ($choices['pel.object.category.multimedia'] === $choice) {
+            $form
+                ->add('brand', TextType::class, [
+                    'constraints' => [
+                        new NotBlank(),
+                        new Length([
+                            'max' => 20,
+                        ]),
+                    ],
+                    'label' => 'pel.brand',
+                ])
+                ->add('model', TextType::class, [
+                    'constraints' => [
+                        new Length([
+                            'max' => 20,
+                        ]),
+                    ],
+                    'label' => 'pel.model',
+                    'required' => false,
+                ])
+                ->add('phoneNumberLine', TextType::class, [
+                    'constraints' => [
+                        new Length([
+                            'max' => 20,
+                        ]),
+                    ],
+                    'label' => 'pel.phone.number.line',
+                    'required' => false,
+                ])
+                ->add('operator', TextType::class, [
+                    'constraints' => [
+                        new Length([
+                            'max' => 20,
+                        ]),
+                    ],
+                    'label' => 'pel.operator',
+                    'required' => false,
+                ])
+                ->add('serialNumber', TextType::class, [
+                    'constraints' => [
+                        new Length([
+                            'max' => 20,
+                        ]),
+                    ],
+                    'help' => 'pel.serial.number.help',
+                    'label' => 'pel.serial.number',
+                    'required' => false,
+                ])
+            ;
+        }
     }
 }
