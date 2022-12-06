14e8d2196f1268382d9a7d0e416efdb8345d0e3a
Thomas CAUTRES <thomas.cautres@sensiolabs.com>
2022-12-06 11:56:07 +0100

    [PEL-513] set amount field input as MoneyType

--- portail_citoyen/src/Form/Objects/ObjectType.php
+++ portail_citoyen/src/Form/Objects/ObjectType.php
@@ -9,6 +9,7 @@ use App\Thesaurus\ObjectCategoryThesaurusProviderInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -307,8 +308,10 @@ class ObjectType extends AbstractType
 
     private function addAmountField(FormInterface $form, string $label = 'pel.amount'): void
     {
-        $form->add('amount', NumberType::class, [
+        $form->add('amount', MoneyType::class, [
             'label' => $label,
+            'scale' => 0,
+            'currency' => false,
             'html5' => true,
             'attr' => [
                 'min' => 1,
