aa4c7a63c3ebbad731c331bed97d1ffb80c830ea
Flavien Aymonnier <flavien.aymonnier@sensiolabs.net>
2023-01-10 14:36:12 +0100

    [FEAT] PEL-549

---
+++ portail_agent/assets/app.ts
@@ -0,0 +1,6 @@
+import "./bootstrap";
+import "bootstrap/dist/js/bootstrap";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./styles/styles.sass";
