58e98b5cc17bf54566d3846216643407a4ec6294
Romain ROSIER <romain.rosier@sensiolabs.com>
2023-04-11 18:30:55 +0200

    Fix env var for Oodrive

--- portail_agent/config/secrets/prod/prod.list.php
+++ portail_agent/config/secrets/prod/prod.list.php
@@ -3,6 +3,9 @@
 return [
     'APP_SECRET' => null,
     'DATABASE_URL' => null,
+    'OODRIVE_OAUTH_CLIENT_SECRET' => null,
+    'OODRIVE_PASSWORD' => null,
+    'OODRIVE_USERNAME' => null,
     'REFERENTIALS_DATABASE_URL' => null,
     'TRUSTED_PROXIES' => null,
 ];
