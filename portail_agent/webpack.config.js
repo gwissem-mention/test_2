be5c020826b68d0fca296530ef7d902943889bf3
Guillaume FORTAINE <guillaume.fortaine@sensiolabs.net>
2023-04-24 16:56:28 +0200

    feat(citoyen/webpack): enable live reload 🎉

--- portail_agent/webpack.config.js
+++ portail_agent/webpack.config.js
@@ -29,6 +29,38 @@ Encore
         {from: "./assets/images", to: "images/[path][name].[hash:8].[ext]"},
         {from: "./assets/json/datatables", to: "json/datatables/[path][name].[ext]"},
     ])
+    // https://symfony.com/doc/current/frontend/encore/dev-server.html
+    .configureDevServerOptions(options => {
+        // https://symfony.com/doc/current/frontend/encore/virtual-machine.html#allow-external-access
+        options.host = '0.0.0.0';
+
+        // https://www.spiriit.com/blog/nginx-webpack-dev-serveur-browsersync-dans-une-stack-docker/
+        options.proxy = {
+            '*': 'http://nginx-agent',
+            secure: false,
+            changeOrigin: true,
+            autoRewrite: true,
+            ignorePath: false,
+            // https://github.com/webpack/webpack-dev-server/issues/793#issuecomment-444165866
+            headers: {
+                Connection: 'Keep-Alive',
+            }
+        }
+
+        // https://symfony.com/doc/current/frontend/encore/virtual-machine.html#fix-invalid-host-header-issue
+        options.allowedHosts = [".pel.localhost"];
+
+        // https://medium.com/@pbrecska/how-to-run-https-webpack-dev-server-in-dockervirtual-enviroment-9238b90fba4c
+        options.headers = {
+            "Access-Control-Allow-Origin": "*",
+        }
+
+        // https://stackoverflow.com/a/68671321
+        // https://stackoverflow.com/a/75365211
+        options.watchFiles = {
+            paths: ['templates/**/*.twig', 'assets/**/*.ts', 'assets/**/*.sass'],
+        }
+    })
 ;
 
 module.exports = Encore.getWebpackConfig();
