fff933f727a05df4e26b0f26e1097d4c7935daf3
Thomas CAUTRES <thomas.cautres@sensiolabs.com>
2023-06-12 15:22:13 +0200

    [PEL-968] Add users for PN

--- portail_agent/src/DataFixtures/UserFixtures.php
+++ portail_agent/src/DataFixtures/UserFixtures.php
@@ -9,9 +9,45 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class UserFixtures extends Fixture implements FixtureGroupInterface
 {
+    private const NB_USERS_BY_UNIT = 2;
+    private const SERVICES = [
+            '68809',
+            '85193',
+            '85194',
+            '68830',
+            '68849',
+            '72903',
+            '102601',
+            '110279',
+            '110280',
+            '110281',
+            '110282',
+            '102645',
+            '102647',
+            '72727',
+            '68905',
+            '74097',
+            '102765',
+            '102812',
+            '102821',
+            '102840',
+            '74181',
+            '65575',
+            '105296',
+            '105297',
+            '66043',
+            '66063',
+            '66082',
+            '105405',
+            '63139',
+            '63169',
+            '63179',
+        ];
+
     public static function getGroups(): array
     {
         return ['default', 'ci'];
@@ -19,26 +55,63 @@ class UserFixtures extends Fixture implements FixtureGroupInterface
 
     public function load(ObjectManager $manager): void
     {
+        $faker = Factory::create('fr_FR');
+        $faker->seed(1);
+
         $users = [
-            (new User('H3U3XCGD', Institution::PN))
-                ->setServiceCode('103131') // CSP VOIRON/SVP/UPS/BRIGADE DE JOUR
-                ->setAppellation('Jean DUPONT'),
-            (new User('PR5KTZ9R', Institution::GN, true)) // Supervisor
-                ->setServiceCode('3002739') // Brigade de proximité de Voiron
-                ->setAppellation('Thomas DURAND'),
-            (new User('H3U3XCGF', Institution::PN, true)) // Supervisor
-                ->setServiceCode('103131') // CSP VOIRON/SVP/UPS/BRIGADE DE JOUR
-                ->setAppellation('Louise THOMAS'),
-            (new User('PR5KTZ9C', Institution::GN))
-                ->setServiceCode('3002739') // Brigade de proximité de Voiron
-                ->setAppellation('Julie RICHARD'),
-            (new User('PR5KTQSD', Institution::GN))
-                ->setServiceCode('3002739') // Brigade de proximité de Voiron
-                ->setAppellation('Philippe RIVIERE'),
+            [
+                'appellation' => 'Jean DUPONT',
+                'number' => 'H3U3XCGD',
+                'institution' => Institution::PN,
+                'serviceCode' => '103131', // CSP VOIRON/SVP/UPS/BRIGADE DE JOUR
+                'supervisor' => false,
+            ],
+            [
+                'appellation' => 'Thomas DURAND',
+                'number' => 'PR5KTZ9R',
+                'institution' => Institution::GN,
+                'serviceCode' => '3002739', // Brigade de proximité de Voiron
+                'supervisor' => true,
+            ],
+            [
+                'appellation' => 'Louise THOMAS',
+                'number' => 'H3U3XCGF',
+                'institution' => Institution::PN,
+                'serviceCode' => '103131', // CSP VOIRON/SVP/UPS/BRIGADE DE JOUR
+                'supervisor' => true,
+            ],
+            [
+                'appellation' => 'Julie RICHARD',
+                'number' => 'PR5KTZ9C',
+                'institution' => Institution::GN,
+                'serviceCode' => '3002739', // Brigade de proximité de Voiron
+                'supervisor' => false,
+            ],
+            [
+                'appellation' => 'Philippe RIVIERE',
+                'number' => 'PR5KTQSD',
+                'institution' => Institution::GN,
+                'serviceCode' => '3002739', // Brigade de proximité de Voiron
+                'supervisor' => false,
+            ],
         ];
 
+        foreach (self::SERVICES as $service) {
+            for ($i = 0; $i < self::NB_USERS_BY_UNIT; ++$i) {
+                $users[] = [
+                    'appellation' => $faker->firstName().' '.strtoupper($faker->lastName()),
+                    'number' => strtoupper($faker->unique()->bothify('????????')),
+                    'institution' => Institution::PN,
+                    'serviceCode' => $service,
+                    'supervisor' => 1 === $i,
+                ];
+            }
+        }
+
         foreach ($users as $user) {
-            $manager->persist($user);
+            $manager->persist((new User($user['number'], $user['institution'], $user['supervisor']))
+                ->setAppellation($user['appellation'])
+                ->setServiceCode($user['serviceCode']));
         }
 
         $manager->flush();
