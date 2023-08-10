036183bce2e3ed2e8103d4528b9c97e6d3787977
Thomas CAUTRES <thomas.cautres@sensiolabs.com>
2023-08-10 14:05:17 +0200

    Fix snappy pdf to work with the HTTP basic auth

--- portail_citoyen/config/secrets/prod/prod.list.php
+++ portail_citoyen/config/secrets/prod/prod.list.php
@@ -6,6 +6,8 @@ return [
     'FRANCE_CONNECT_BASE_URI' => null,
     'FRANCE_CONNECT_CLIENT_ID' => null,
     'FRANCE_CONNECT_CLIENT_SECRET' => null,
+    'HTTP_BASIC_AUTH_PASSWORD' => null,
+    'HTTP_BASIC_AUTH_USERNAME' => null,
     'OODRIVE_OAUTH_CLIENT_SECRET' => null,
     'OODRIVE_PASSWORD' => null,
     'OODRIVE_USERNAME' => null,
