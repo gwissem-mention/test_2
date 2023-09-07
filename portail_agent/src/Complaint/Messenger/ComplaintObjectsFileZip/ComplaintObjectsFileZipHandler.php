7406c2135ac64096618b9c8c5a5f7d4b8674aaa2
Romain ROSIER <romain.rosier@sensiolabs.com>
2023-09-07 08:39:17 +0200

    [FIX][PEL-1235] Fix error 500 when a complaint is sending to LRP with no files attached (zip generation)

--- portail_agent/src/Complaint/Messenger/ComplaintObjectsFileZip/ComplaintObjectsFileZipHandler.php
+++ portail_agent/src/Complaint/Messenger/ComplaintObjectsFileZip/ComplaintObjectsFileZipHandler.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Complaint\Messenger\ComplaintObjectsFileZip;
 
 use App\Entity\Complaint;
+use App\Entity\FactsObjects\AbstractObject;
 use App\Repository\ComplaintRepository;
 use League\Flysystem\FilesystemOperator;
 use Symfony\Component\Filesystem\Filesystem;
@@ -25,24 +26,27 @@ class ComplaintObjectsFileZipHandler
         /** @var Complaint $complaint */
         $complaint = $this->complaintRepository->find($complaintObjectsFileZipMessage->getComplaintId());
 
+        if (!$complaint->hasObjectsWithFiles()) {
+            return;
+        }
+
         $zip = new \ZipArchive();
         $tmpZipName = $this->filesystem->tempnam(sys_get_temp_dir(), 'sb_');
         $zip->open($tmpZipName, \ZipArchive::CREATE);
 
-        foreach ($complaint->getObjects() as $object) {
-            if (($objects = $object->getFiles()) != null) {
-                foreach ($objects as $file) {
-                    $fileStream = $this->defaultStorage->readStream($file);
-                    $tmpFileName = $this->filesystem->tempnam(sys_get_temp_dir(), 'sb_');
-                    /** @var resource $tmpFile */
-                    $tmpFile = fopen($tmpFileName, 'wb+');
-                    stream_copy_to_stream($fileStream, $tmpFile);
+        /** @var AbstractObject $object */
+        foreach ($complaint->getObjectsWithFiles() as $object) {
+            foreach ($object->getFiles() ?? [] as $file) {
+                $fileStream = $this->defaultStorage->readStream($file);
+                $tmpFileName = $this->filesystem->tempnam(sys_get_temp_dir(), 'sb_');
+                /** @var resource $tmpFile */
+                $tmpFile = fopen($tmpFileName, 'wb+');
+                stream_copy_to_stream($fileStream, $tmpFile);
 
-                    $zip->addFile($tmpFileName, basename($file));
-                }
+                $zip->addFile($tmpFileName, basename($file));
+                fclose($tmpFile);
             }
         }
-
         $zip->close();
 
         $this->defaultStorage->writeStream(
