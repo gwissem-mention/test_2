20ee9cd8f3ca2ea3f0664a56e3a3f4b8f6ade8a1
Thomas CAUTRES <thomas.cautres@sensiolabs.com>
2023-09-26 17:49:07 +0200

    [PEL-1372] Update Various section XML tags

--- portail_agent/src/Generator/Complaint/Model/VariousDTO.php
+++ portail_agent/src/Generator/Complaint/Model/VariousDTO.php
@@ -9,12 +9,7 @@ use App\Referential\Repository\UnitRepository;
 
 class VariousDTO
 {
-    private string $franceConnected;
-    private string $createdAt;
-    private string $declarationNumber;
     private string $exactDateKnown;
-    private string $place;
-    private string $addressAdditionalInformation;
     private string $suspectsKnown;
     private string $suspectsKnownText;
     private string $witnessesPresent;
@@ -23,19 +18,11 @@ class VariousDTO
     private string $cctvAvailable;
     private string $appointmentUnit;
     private string $appointmentAsked;
-    private string $job;
     private string $corporationContactEmail;
-    private string $callingPhone;
-    private string $factsWebsite;
 
     public function __construct(Complaint $complaint, UnitRepository $unitRepository)
     {
-        $this->franceConnected = $complaint->isFranceConnected() ? 'Oui' : 'Non';
-        $this->createdAt = $complaint->getCreatedAt()?->format('d/m/Y H:i:s') ?? '';
-        $this->declarationNumber = $complaint->getDeclarationNumber();
         $this->exactDateKnown = $complaint->getFacts()?->isExactDateKnown() ? 'Oui' : 'Non';
-        $this->place = $complaint->getFacts()?->getPlace() ?? '';
-        $this->addressAdditionalInformation = $complaint->getFacts()?->getAddressAdditionalInformation() ?? '';
         $this->suspectsKnown = $complaint->getAdditionalInformation()?->isSuspectsKnown() ? 'Oui' : 'Non';
         $this->suspectsKnownText = $complaint->getAdditionalInformation()?->getSuspectsKnownText() ?? '';
         $this->witnessesPresent = $complaint->getAdditionalInformation()?->isWitnessesPresent() ? 'Oui' : 'Non';
@@ -43,11 +30,8 @@ class VariousDTO
         $this->observationMade = $complaint->getAdditionalInformation()?->isObservationMade() ? 'Oui' : 'Non';
         $this->cctvAvailable = $complaint->getAdditionalInformation()?->isCctvAvailable() ? 'Oui' : 'Non';
         $this->appointmentAsked = $complaint->isAppointmentAsked() ? 'Oui' : 'Non';
-        $this->job = $complaint->getIdentity()?->getJob() ?? '';
         $this->corporationContactEmail = $complaint->getCorporationRepresented()?->getContactEmail() ?? '';
         $this->appointmentUnit = $unitRepository->findOneBy(['code' => $complaint->getUnitAssigned()])?->getName() ?? '';
-        $this->callingPhone = $complaint->getFacts()?->getCallingPhone() ?? '';
-        $this->factsWebsite = $complaint->getFacts()?->getWebsite() ?? '';
     }
 
     /**
@@ -56,24 +40,16 @@ class VariousDTO
     public function getArray(): array
     {
         return ['Divers' => [
-            'DIVERS_FRANCE_CONNECTE' => $this->franceConnected,
-            'DIVERS_SOUMISSION_HORAIRE' => $this->createdAt,
-            'DIVERS_NUMERO_PEL' => $this->declarationNumber,
             'DIVERS_DATE_EXACTE_FAITS_CONNUE' => $this->exactDateKnown,
-            'DIVERS_NATURE_LIEU' => $this->place,
-            'DIVERS_LIEU_INFORMATION_COMPLEMENTAIRES' => $this->addressAdditionalInformation,
-            'DIVERS_SUSPECTS_INFORMATIONS' => $this->suspectsKnown,
-            'DIVERS_SUSPECTS_DESCRIPTION' => $this->suspectsKnownText,
-            'DIVERS_TEMOINS_PRESENTS' => $this->witnessesPresent,
-            'DIVERS_INTERVENTION_FSI' => $this->fsiVisit,
-            'DIVERS_CONSTAT_RELEV_EFFECTUES' => $this->observationMade,
-            'DIVERS_VIDEO_DISPONIBLE' => $this->cctvAvailable,
-            'DIVERS_UNITE_RDV' => $this->appointmentUnit,
-            'DIVERS_RDV_SOUHAITE' => $this->appointmentAsked,
-            'DIVERS_PROFESSION_PEL' => $this->job,
+            'SUSPECTS_INFORMATIONS' => $this->suspectsKnown,
+            'SUSPECTS_DESCRIPTION' => $this->suspectsKnownText,
+            'TEMOINS_PRESENTS' => $this->witnessesPresent,
+            'INTERVENTION_FSI' => $this->fsiVisit,
+            'CONSTAT_RELEV_EFFECTUES' => $this->observationMade,
+            'VIDEO_DISPONIBLE' => $this->cctvAvailable,
+            'UNITE_RDV' => $this->appointmentUnit,
+            'RDV_SOUHAITE' => $this->appointmentAsked,
             'Mail_Personne_Morale' => $this->corporationContactEmail,
-            'TEL_APPELANT' => $this->callingPhone,
-            'NATURE_LIEU_URL' => $this->factsWebsite,
         ]];
     }
 }
