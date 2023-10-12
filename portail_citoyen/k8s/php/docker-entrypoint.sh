013ccaa23e62301d8b5adb0a9493e9a054960a49
Salah MEHARGA <salah.meharga@sensiolabs.com>
2023-10-12 12:33:02 +0200

    Anfsi deployment

--- portail_citoyen/k8s/php/docker-entrypoint.sh
+++ portail_citoyen/k8s/php/docker-entrypoint.sh
@@ -8,6 +8,11 @@ if [ "${1#-}" != "$1" ]; then
   set -- php-fpm "$@"
 fi
 
+sed -i "s/REDIS_PASSWORD=/REDIS_PASSWORD=${REDIS_PASSWORD}/g" .env
+sed -i "s/REFERENTIALS_DATABASE_URL==/REFERENTIALS_DATABASE_URL=${REFERENTIALS_DATABASE_URL}/g" .env
+#sed -i "s/REDIS_HOST=/REDIS_HOST=${REDIS_HOST}/g" .env
+
+
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 
   composer run-script --no-dev post-install-cmd
@@ -16,7 +21,7 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
   if true; then
 
     if [ "$( find ./migrations -iname '*.php' -print -quit )" ]; then
-      bin/console doctrine:migrations:migrate --no-interaction
+      bin/console doctrine:migrations:migrate --no-interaction --env=prod
     fi
   fi
 
