5b4344b66469463a019f76ff80347a694913ae7a
Romain ROSIER <romain.rosier@sensiolabs.com>
2023-10-23 15:23:12 +0200

    [PEL-1557] Add try/catch to complaint fetcher cron

--- portail_agent/src/Command/CRON/ComplaintFetchCron.php
+++ portail_agent/src/Command/CRON/ComplaintFetchCron.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Command\CRON;
 
 use App\Complaint\ComplaintFetcher;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -17,17 +18,24 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class ComplaintFetchCron extends Command
 {
-    public function __construct(private readonly ComplaintFetcher $complaintFetcher)
-    {
+    public function __construct(
+        private readonly ComplaintFetcher $complaintFetcher,
+        private readonly LoggerInterface $logger,
+    ) {
         parent::__construct(self::getDefaultName());
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $io->title('Fetch complaints');
 
-        $complaintsFetchedCount = $this->complaintFetcher->fetch();
+        $complaintsFetchedCount = 0;
+
+        try {
+            $complaintsFetchedCount = $this->complaintFetcher->fetch();
+        } catch (\Exception  $e) {
+            $this->logger->error(sprintf('Failed to fetch complaints. Error: %s', $e->getMessage()));
+        }
 
         $io->note(sprintf('%s complaints fetched.', $complaintsFetchedCount));
 
