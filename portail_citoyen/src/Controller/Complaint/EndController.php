9d10ccdc6cab92a861e02f7c6095a6c86cf395c9
Thomas CAUTRES <thomas.cautres@sensiolabs.com>
2023-11-17 12:44:28 +0100

    [PEL-1626] Clear session after oodrive push

--- portail_citoyen/src/Controller/Complaint/EndController.php
+++ portail_citoyen/src/Controller/Complaint/EndController.php
@@ -7,20 +7,26 @@ namespace App\Controller\Complaint;
 use App\Form\Model\AdditionalInformation\AdditionalInformationModel;
 use App\Session\SessionHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class EndController extends AbstractController
 {
     #[Route('/porter-plainte/fin', name: 'complaint_end')]
-    public function __invoke(SessionHandler $sessionHandler): Response
+    public function __invoke(SessionHandler $sessionHandler, Request $request, SerializerInterface $serializer): Response
     {
         if (!$sessionHandler->getComplaint()?->getAdditionalInformation() instanceof AdditionalInformationModel) {
             return $this->redirectToRoute('home');
         }
 
+        $complaint = $sessionHandler->getComplaint();
+        $request->getSession()->clear();
+
         return $this->render('pages/complaint_end.html.twig', [
-            'complaint' => $sessionHandler->getComplaint(),
+            'complaint' => $complaint,
+            'complaint_json' => $serializer->serialize($complaint, 'json'),
         ]);
     }
 }
